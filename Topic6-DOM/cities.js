25a4cd31b435c475a697c489b7887142688d8952
Rikkokiri <pilvi.rajala@gmail.com>
2017-04-03 16:40:56 +0300

    Creating list of available cities

---
+++ Topic6-DOM/cities.js
@@ -0,0 +1,223 @@
+// https://en.wikipedia.org/wiki/Finnair_destinations
+// http://aa.fltmaps.com/en
+// https://en.wikipedia.org/wiki/American_Airlines_destinations
+
+var northAmerica = [
+  //Canada
+
+
+  // USA by state
+  //Alabama
+  //Alaska
+  "Fairbanks", "Anchorage",
+  //Arizona
+
+  //Arkansas
+  //California
+  //Colorado
+  //Connecticut
+  //Delaware
+  //Florida
+  //Georgia
+  //Hawaii
+  //Idaho
+  //Illinois Indiana
+  //Iowa
+  //Kansas
+  //Kentucky
+  //Louisiana
+  //Maine
+  //Maryland
+  //Massachusetts
+  //Michigan
+  //Minnesota
+  //Mississippi
+  //Missouri
+  //Montana Nebraska
+  //Nevada
+  //New Hampshire
+  //New Jersey
+  //New Mexico
+  //New York
+  //North Carolina
+  //North Dakota
+  //Ohio
+  //Oklahoma
+  //Oregon
+  //Pennsylvania Rhode Island
+  //South Carolina
+  //South Dakota
+  //Tennessee
+  //Texas
+  //Utah
+  //Vermont
+  //Virginia
+  //Washington
+  //West Virginia
+  //Wisconsin
+  //Wyoming
+];
+
+var mexico = [
+
+];
+
+var southAmerica = [
+  //Argentina - Buenos Aires
+  //Bolivia - La Paz
+  //Brazil - Brasilia
+  //Chile - Santiago
+  //Colombia - Bogotá
+  //Ecuador - Quito
+  //Guyana - Georgetown
+  //Paraguay - Asunción
+  //Peru - Lima
+  //Suriname - Paramaribo
+  //Uruguay - Montevideo
+  //Venezuela - Caracas
+];
+
+var europe = [
+  //Austria
+  "Vienna",
+  //Belgium
+  "Brussel",
+  //Bulgaria
+  "Sofia",
+  //Croatia
+  "Zagreb", "Dubrovnik", "Pula", "Split"
+  //Cyprus
+  "Larnaca",
+  //Czech Republic
+  "Prague",
+  //Denmark
+  "Copenhagen",
+  //Finland
+  "Helsinki",
+  //France
+  "Paris", "Nice", "Lyon", "Bordeaux", "Toulouse", "Marseille",
+  //Germany
+  "Berlin", "Cologne", "Düsseldorf", "Dusseldorf", "Frankfurt", "Hamburg", "Hannover" "Munich", "Nuremberg", "Stuttgart",
+  //Greece
+  "Athens",
+  //Hungary
+  "Budapest",
+  //Iceland
+  "Reykjavik",
+  //Ireland
+  "Dublin",
+  //Italy
+  "Rome", "Milan", "Venice", "Bologna", "Pisa", "Naples",
+  //Latvia
+  "Riga",
+  //Lithuania
+  "Vilnius",
+  //Luxembourg
+  "Luxembourg",
+  //Malta
+  "Valletta",
+  //Netherlands
+  "Amsterdam",
+  //Norway
+  "Oslo", "Tromsø", "Tromso",
+  //Poland
+  "Warsaw", "Gdańsk", "Gdansk", "Kraków", "Krakow",
+  //Portugal
+  "Lisbon", "Porto",
+  //Romania
+  "Bucharest",
+  //Russia
+  "Moscow", "Murmansk", "Saint Petersburg",
+  //Slovenia
+  "Ljubljana",
+  //Spain
+  "Madrid", "Malaga", "Granada", "Barcelona", "Alicante", "Valencia", "Asturias", "Pamplona",
+  "Lanzarote", "Gran Canaria", "Fuerteventura", "Ibiza", "Menorca", "Mallorca", "Tenerife",
+  //Sweden
+  "Stockholm",
+  //Switzerland
+  "Zurich", "Geneva",
+  //Turkey
+  "Istanbul",
+  //Ukraine
+  "Kiev",
+  //United Kingdom
+  "London", "Manchester", "Edinburgh", "Glasgow", "Newcastle", "Belfast",
+];
+
+var asia = [
+  //Afghanistan - Kabul
+  //Armenia - Yerevan
+  //Azerbaijan - Baku
+  //Bahrain - Manama
+  //Bangladesh - Dhaka
+  //Bhutan - Thimphu
+  //Brunei - Bandar Seri Begawan
+  //Cambodia - Phnom - Penh
+  //China - Beijing
+  //Cyprus - Nicosia
+  //Georgia - Tbilisi
+  //Hong - Kong
+  //India - New - Delhi
+  //Indonesia - Jakarta
+  //Iran - Tehran
+  //Iraq - Baghdad
+  //Israel - Jerusalem
+  //Japan - Tokyo
+  //Jordan - Amman
+  //Kazakhstan - Astana
+  //South - Korea - Seoul
+  //North - Korea - Pyongyang
+  //Kuwait - Kuwait - City
+  //Kyrgyzstan - Bishkek
+  //Laos - Vientiane
+  //Lebanon - Beirut
+  //Macau
+  //Malaysia - Kuala Lumpur
+  //Maldives - Malé
+  //Mongolia - Ulan - Bator
+  //Myanmar - Naypyidaw
+  //Nepal - Kathmandu
+  //Oman - Muscat
+  //Pakistan - Islamabad
+  //Palestinian National Authority - Ramallah
+  //Philippines - Manila
+  //Qatar - Doha
+  //Saudi Arabia - Riyadh
+  //Singapore - Singapore
+  //Sri Lanka - Sri Jayawardenapura Kotte
+  //Syria - Damascus
+  //Tajikistan - Dushanbe
+  //Thailand - Bangkok
+  //Timor-Leste - Dili
+  //Turkey - Ankara
+  //Turkmenistan - Ashgabat
+  //United Arab Emirates - Abu Dhabi
+  //Uzbekistan - Tashkent
+  //Vietnam - Hanoi
+  //Yemen - Sana'a
+  //Abkhazia - Sukhumi
+  //Taiwan - Taipei
+  //Nagorno-Karabakh Republic - Stepanakert
+  //Northern Cyprus - North Nicosia
+  //South Ossetia - Tskhinvali
+];
+
+var africa = [
+  //Morocco
+  "Agadir", "Marrakech",
+  //Egypt
+  "Cairo", "Hurghada", "Marsam Alam",
+  //Kenya
+  "Nairobi"
+  //Ethiopia
+  "Addis Abbada",
+  //Tanzania
+  "Dar Es Salaam",
+  //South Africa
+  "Cape Town", "Johannesburg",
+  //Ghana
+  "Accra",
+  //Senegal
+  "Dakar"
+];
